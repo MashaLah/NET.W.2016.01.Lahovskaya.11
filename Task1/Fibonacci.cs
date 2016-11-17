c74c75bf6b564fcb4f715a11803611297be30a4a
MashaLah <lahoska91@mail.ru>
2016-11-17 13:56:02 +0300

    Add Task1.

---
+++ Task1/Fibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// Generates Fibonacci numbers.
+        /// </summary>
+        /// <param name="n">Number of Fibonacci numbers</param>
+        /// <exception  cref="ArgumentOutOfRangeException">
+        /// Throws if n<2.
+        /// </exception>
+        /// <returns>Chain of Fibonacci numbers.</returns>
+        public static IEnumerable<int> GenerateFibonacciNumbers(int n)
+        {
+            if (n < 2) throw new ArgumentOutOfRangeException(nameof(n));
+            int previous = 0;
+            int current = 1;
+            yield return previous;
+            yield return current;
+            int next;
+            for (int i = 0; i < n; i++)
+            {
+                yield return next = previous + current;
+                previous = current;
+                current = next;
+            }
+        }
+    }
+}
---
+++ Task1Tests/FibonacciTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Task1.Fibonacci;
+using NUnit.Framework;
+
+namespace Task1Tests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        public void GenerateFibonacciNumbers_ValidData_ValidResult()
+        {
+            foreach (int i in GenerateFibonacciNumbers(5))
+            {
+                Console.Write($"{i} ");
+            }
+        }
+
+        [TestCase(-5)]
+        [TestCase(0)]
+        public void GenerateFibonacciNumbers_InvalidData_ThrowException(int n)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => GenerateFibonacciNumbers(n));
+        }
+    }
+}
